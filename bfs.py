ab148224b323c47582dec25ae59e06440760ed30
C-A-T <cat9life@yandex.ru>
2016-01-24 04:59:19 +0200

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -3,7 +3,7 @@ while True:
     import winsound         # for sound :)
     from itertools import product
     for i, item in enumerate(product('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_',repeat=4)): # перебираемые символы и их количество 
-     s = (str("http://www."+''.join(item)+ '.jpg' )) #адрес сайта и при необходимости расширение файла
+     s = (str("http://www."+''.join(item)+ '.jpg' )) #адрес сайта и расширение файла
      r = requests.head(s)
      print (str((r.status_code))+s)
      if r.status_code == 200:
